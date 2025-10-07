023df43eecccca23f83fd68d6a230820c0c8fc37
Lendrock <lchoxon-2021286@kinal.edu.gt>
2025-10-07 07:57:49 -0600

    T1/ Archivos iniciales para el proyecto

---
+++ src/test/java/org/albor/fentfood/FentfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package org.albor.fentfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FentfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
